b49daf506b71ea3f7c3adbf65a95d3bb48cb54ee
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-25 15:48:06 +0200

    CI: Update Storybook config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
+  staticDirs: ["../public", "../src/assets"],
   core: {
     builder: "@storybook/builder-vite",
     disableTelemetry: true,
