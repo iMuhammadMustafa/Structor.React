c4c84f29c1281a0f2b66e6682b238e6e2ea66f71
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-26 19:12:43 +0200

    Add Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/scss/bootstrap.scss";
+
+import App from "./App";
+import "./main.scss";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
